295815843ec52cf0e8bf5b2e15470cabcc0b2c60
jznxm <keithkun93@gmail.com>
2021-06-06 08:48:58 +0800

    Remove directory FILES because compatible problem

--- Code/FILES/MY_DEL.C
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-void main(int argc, char *argv[])
- {
-   while (*++argv)
-     if (remove(*argv))
-       printf("Error removing %s\n", *argv);
- }
--- Code/FILES/UNLINK.C
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-void main(int argc, char *argv[])
- {
-   while (*++argv)
-     if (unlink(*argv))
-       printf("Error removing %s\n", *argv);
- }
