c5c04a20887523b2eb38ef1d5a20d04aa30f3f6a
jznxm <keithkun93@gmail.com>
2021-06-07 08:47:20 +0800

    Change program to modern C programming style

--- Code/KEYBOARD/CGETS.C
+++ Code/KEYBOARD/CGETS.C
@@ -1,18 +1,17 @@
 #include <stdio.h>
-#include <conio.h>
 
-void main(void)
- {
-   char buffer[256];
+int main(void)
+{
+  char buffer[256];
 
-   buffer[0] = 253;  /* Number of characters that can be read*/
+  buffer[0] = 253; /* Number of characters that can be read*/
 
-   printf("Type in a string and press Enter\n");
-   cgets(buffer);
+  printf("Type in a string and press Enter\n");
+  fgets(buffer, 256, stdin);
 
-   printf("\n\nThe number of characters read was %d\n",
-     buffer[1]);
+  printf("\n\nThe number of characters read was %d\n",
+         buffer[1]);
 
-   printf("The string read: %s\n", &buffer[2]);
- }
-
+  printf("The string read: %s\n", &buffer[2]);
+  return 0;
+}
