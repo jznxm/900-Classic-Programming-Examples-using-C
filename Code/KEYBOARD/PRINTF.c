aea2abc723dc0424d7a565b078f82c8fb13c3949
jznxm <keithkun93@gmail.com>
2021-06-07 08:57:09 +0800

    Rename CPRINTF.c with PRINTF.c

---
+++ Code/KEYBOARD/PRINTF.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <time.h>
+
+int main(void)
+{
+  int count;
+
+  time_t start_time, stop_time;
+
+  time(&start_time);
+  for (count = 0; count < 1000001; count++)
+    printf("Jamsa's 1001 C/C++ Tips\n");
+  time(&stop_time);
+
+  printf("\n\nTime required for printf %ld seconds\n",
+         stop_time - start_time);
+  printf("Press any key...\n");
+  getchar();
+
+  time(&start_time);
+  for (count = 0; count < 1000001; count++)
+    printf("Jamsa's 1001 C/C++ Tips\r\n");
+
+  time(&stop_time);
+
+  printf("\n\nTime required for cprintf %ld seconds\n",
+         stop_time - start_time);
+  return 0;
+}
