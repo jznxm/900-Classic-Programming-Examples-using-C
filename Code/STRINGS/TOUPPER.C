7af4e75f53d0d0cb55622514a945a2a7914c9d7b
jznxm <keithkun93@gmail.com>
2021-06-08 14:55:08 +0800

    Change to modern C programming style

--- Code/STRINGS/TOUPPER.C
+++ Code/STRINGS/TOUPPER.C
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <ctype.h>
 
-void main (void)
- {
-   char string[] = "Jamsa's 1001 C & C++ Tips";
+int main(void)
+{
+  char string[] = "Jamsa's 1001 C & C++ Tips";
 
-   int i;
+  int i;
 
-   for (i = 0; string[i]; i++)
-     putchar(toupper(string[i]));
-   putchar('\n');
+  for (i = 0; string[i]; i++)
+    putchar(toupper(string[i]));
+  putchar('\n');
 
-   for (i = 0; string[i]; i++)
-     putchar(_toupper(string[i]));
-   putchar('\n');
- }
+  for (i = 0; string[i]; i++)
+    putchar(_toupper(string[i]));
+  putchar('\n');
+  return 0;
+}
