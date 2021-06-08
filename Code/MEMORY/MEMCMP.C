d7ca36188cff59a22696040c30c994077ab42a2a
jznxm <keithkun93@gmail.com>
2021-06-08 14:32:58 +0800

    Change MEMCMP.C to modern C programming style

--- Code/MEMORY/MEMCMP.C
+++ Code/MEMORY/MEMCMP.C
@@ -1,16 +1,13 @@
 #include <stdio.h>
-#include <mem.h>
+#include <string.h>
 
-void main(void)
- {
-   char *a = "AAA";
-   char *b = "BBB";
-   char *c = "aaa";
+int main(void)
+{
+  char *a = "AAA";
+  char *b = "BBB";
 
-   printf("Comparing %s and %s with memcmp %d\n",
-     a, b, memcmp(a, b, sizeof(a)));
+  printf("Comparing %s and %s with memcmp %d\n",
+         a, b, memcmp(a, b, sizeof(a)));
 
-   printf("Comparing %s and %s with memicmp %d\n",
-     a, b, memicmp(a, c, sizeof(a)));
- }  
- 
+  return 0;
+}
