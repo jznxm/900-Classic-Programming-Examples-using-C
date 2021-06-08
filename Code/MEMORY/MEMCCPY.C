232350912bbd4bd352f9c31b8b3efd8934b8966c
jznxm <keithkun93@gmail.com>
2021-06-08 14:29:36 +0800

    Change MEMCCPY.C to modern C programming style

--- Code/MEMORY/MEMCCPY.C
+++ Code/MEMORY/MEMCCPY.C
@@ -1,17 +1,15 @@
 #include <stdio.h>
-#include <mem.h>
+#include <string.h>
 
-void main(void)
- {
-   char alphabet[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-   char target[27];
-   char *result;
+int main(void)
+{
+  char alphabet[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  char target1[27], target2[27];
 
-   result = memccpy(target, alphabet, 'K', sizeof(alphabet));
-   
-   if (result)
-     *result = NULL;
+  memccpy(target1, alphabet, 'E', sizeof(alphabet));
+  memcpy(target2, alphabet, sizeof(alphabet));
 
-   printf(target);
- }
-
+  printf("target1: %s\n", target1);
+  printf("target2: %s\n", target2);
+  return 0;
+}
