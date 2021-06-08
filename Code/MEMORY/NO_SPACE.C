f31bd4c04ccb9a2d2c3b1472752c474a7717bf50
jznxm <keithkun93@gmail.com>
2021-06-08 14:35:50 +0800

    Change NO_SPACE.C to modern C programming style

--- Code/MEMORY/NO_SPACE.C
+++ Code/MEMORY/NO_SPACE.C
@@ -1,16 +1,17 @@
 #include <stdio.h>
-#include <alloc.h>
+#include <stdlib.h>
 
-void main(void)
- {
-   char *one, *two, *three;
+int main(void)
+{
+  char *one, *two, *three;
 
-   if ((one = (char *) malloc(30000)) == NULL)
-     printf("Error allocating array one\n");
-   else if ((two = (char *) malloc(30000)) == NULL)
-     printf("Error allocating array two\n");
-   else if ((three = (char *) malloc(30000)) == NULL)
-     printf("Error allocating array three\n");
-   else
-     printf("All arrays successfully allocated\n");
- }
+  if ((one = (char *)malloc(30000)) == NULL)
+    printf("Error allocating array one\n");
+  else if ((two = (char *)malloc(30000)) == NULL)
+    printf("Error allocating array two\n");
+  else if ((three = (char *)malloc(30000)) == NULL)
+    printf("Error allocating array three\n");
+  else
+    printf("All arrays successfully allocated\n");
+  return 0;
+}
